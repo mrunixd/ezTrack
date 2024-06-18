6ed13cfd56540d022f529b87055aa3a4e89206af
c-ientific <z5481212@ad.unsw.edu.au>
2024-06-18 11:43:59 +1000

    fixed ui for login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.google.firebase.auth)
+    implementation(libs.gms.play.services.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
