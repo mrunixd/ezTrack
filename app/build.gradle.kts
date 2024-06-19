be5dd114bbc06ec441a1387ebd0240c8efe033b6
c-ientific <z5481212@ad.unsw.edu.au>
2024-06-19 23:03:39 +1000

    completed implementation of google login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.google.firebase.auth)
     implementation(libs.gms.play.services.auth)
+    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
